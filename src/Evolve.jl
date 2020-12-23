fd2e4aa3513f1f843012cbac8e770ddc09ad17cc
Alden Wright <github@fastmail.us>
2020-12-23 07:07:01 -0700

    Wrote mutational_evolvability.jl to find the number of goals seen in num_mutations from a circuit maping to a given goal.

--- src/Evolve.jl
+++ src/Evolve.jl
@@ -2,6 +2,7 @@ export components_matched, goals_matched_exact, goals_matched_hamming, next_chro
 export mut_reduce_numactive
 export random_neutral_walk, match_score, findmaxall, findminall, findmaxall_1, findminall_1, findmaxrand, findminrand
 export evolve_function, mut_evolve_repeat, circuit_evolve, run_circuit_evolve
+export neutral_evolution, geno_circuits, geno_properties, geno_list_properties
 
 # 5/21: To test:
 # ni = 2; nc = 4
@@ -814,3 +815,131 @@ function test_evolve()
   circuit_evolve(c,c_dest, 1000 )
 end
 
+
+# Evolves a chromosome (cirucit) that maps to g starting with chromosome c.
+# max_steps is the maximum number of evolutionary steps.
+# If evolution hasn't succeeeded in max_steps, return nothing.
+# Similar to mut_evolve except that this takes a single goal instead of a goal list as an argument.
+function neutral_evolution( c::Chromosome, g::Goal, max_steps::Integer; print_steps::Bool=false )
+  funcs = default_funcs( c.params.numinputs )
+  step = 0
+  ov = output_values( c) 
+  current_distance = hamming_distance( ov, g, c.params.numinputs )
+  while step < max_steps && ov != g
+    step += 1
+    (new_c,active) = mutate_chromosome!( deepcopy(c), funcs )
+    new_ov = output_values( new_c )
+    new_distance = hamming_distance( new_ov, g, c.params.numinputs )
+    #println("step: ",step,"  ov: ",ov,"  new_ov: ",new_ov,"  cur dis: ",current_distance,"  new_dis: ",new_distance )
+    if new_ov == ov 
+      c = new_c
+      if print_steps
+        println("step: ",step," is neutral.")
+      end
+    elseif new_distance < current_distance
+      if print_steps
+        println("step: ",step,"  new_output: ",new_ov," distance improved from ",current_distance," to ",new_distance)
+      end
+      c = new_c
+      ov = new_ov
+      current_distance = new_distance
+    else
+      if print_steps
+        print("step: ",step,"  new_output: ",new_ov,"  new circuit: ")
+        print_circuit( new_c )
+      end 
+    end
+  end
+  if step == max_steps
+    println("neutral evolution failed with ",step," steps for goal: ",g)
+    return (nothing, step)
+  else
+    println("neutral evolution succeeded at step ",step," for goal: ",g)
+    @assert output_values(c) == g
+    return (c, step)
+  end
+end
+
+# Computes phenotype evolvability, genotype evolvability, robustness, complexity, steps
+# num_circuits is the number of circuits used to compute properties.
+function geno_circuits( g::Goal, p::Parameters, num_circuits::Integer, max_steps::Integer, max_attempts::Integer )
+  funcs = default_funcs( p.numinputs )
+  c = random_chromosome( p, funcs )
+  sum_steps = 0
+  circuit_list = Chromosome[]
+  n_circuits = 0
+  attempt = 0
+  nc = nothing
+  while attempt < max_attempts && n_circuits < num_circuits
+    attempt += 1
+    (nc,steps) = neutral_evolution( c, g, max_steps )
+    sum_steps += steps
+    if nc != nothing
+      n_circuits += 1
+      push!( circuit_list, nc )
+    end
+  end
+  if n_circuits < num_circuits
+    println("geno_properties failed to find num_circuits circuits mapping to goal: ",g," in ", attempt," attempts.")
+    return (nothing, sum_steps)
+  end
+  return (circuit_list, sum_steps)
+end
+ 
+function geno_properties( cl_ss::Tuple{Vector{Chromosome}, Int64} ) # Tuple of circuit list and sum_steps.
+  (cl, sum_steps) = cl_ss
+  funcs = default_funcs( cl[1].params.numinputs )
+  g = output_values(cl[1])
+  sum_robust = 0.0
+  sum_complexity = 0.0
+  sum_evolvability = 0.0  # genotype evolability
+  genotypes = Goal[]
+  genotype_set = Set(Goal[])
+  n_circuits = length(cl)
+  for c in cl
+    @assert output_values(c) == g
+    sum_robust += mutational_robustness( c, funcs )
+    sum_complexity += complexity5(c) 
+    genotypes = Set(mutate_all(c,funcs,output_outputs=true,output_chromosomes=false))
+    sum_evolvability += length(genotypes)-1
+    #println("len(genotypes): ",length(genotypes),"  genotypes: ",genotypes)
+    genotype_set = union( genotype_set, genotypes )
+  end
+  (g, sum_robust/n_circuits, sum_complexity/n_circuits, sum_evolvability/n_circuits, length(genotype_set), sum_steps/n_circuits) 
+end
+
+# Computes the properites robustness, complexity, genotype evolability, phenotype evolvability, evolutonary steps
+#    for the goals of gl.  The properties are based on the evoution of num_circuits circuits that map to the goal.
+# max_attempts is the maximum number of calls to neutral_evolution() that are made in the attempt to evolve num_circuits circuits
+#    that evolve to a goal 
+# max_steps is the maximum number of evolutionary steps done by neutral_evoluion() during 1 attempt to evolve a circuit
+#    that maps to a goal.
+function geno_list_properties( gl::GoalList, p::Parameters, num_circuits::Integer, max_steps::Integer, max_attempts::Integer;
+    csvfile::String="" )
+  df = DataFrame()
+  df.goal = Goal[]
+  df.robustness = Float64[]
+  df.complexity = Float64[]
+  df.g_evolvability = Float64[]
+  df.p_evolvability = Float64[]
+  df.steps = Float64[]
+  gp_list = Vector{Tuple{Goal,Float64,Float64,Float64,Float64,Float64}}[]
+  gp_list = pmap( g->geno_properties( geno_circuits( g, p, num_circuits, max_steps, max_attempts ) ), gl )
+  for gp in gp_list
+    push!(df, gp) 
+  end
+  if length(csvfile) > 0
+    open( csvfile, "w" ) do f
+      hostname = chomp(open("/etc/hostname") do f read(f,String) end)
+      println(f,"# date and time: ",Dates.now())
+      println(f,"# host: ",hostname," with ",nprocs()-1,"  processes: " )
+      println(f,"# funcs: ", Main.CGP.default_funcs(p.numinputs))
+      print_parameters(f,p,comment=true)
+      println(f,"# num_circuits: ",num_circuits)
+      println(f,"# max_steps: ",max_steps)
+      println(f,"# max_attempts: ",max_attempts)
+      CSV.write( f, df, append=true, writeheader=true )
+    end
+  end
+  df
+end
---
+++ src/mutational_evolvability.jl
@@ -0,0 +1,73 @@
+using DataFrames
+using CSV
+
+function mutational_evolvability( c::Chromosome, num_mutations::Int64 )
+  funcs = default_funcs( c.params.numinputs )
+  goal_sets = Set{Goal}[]
+  circuit_set = Set{Chromosome}[]
+  goal_set = Set( Goal[] )
+  for i = 1:num_mutations
+    if i == 1
+      goal_circuit_pairs = mutate_all( c, funcs, output_chromosomes=true ) 
+      circuit_set = Set( map( x->x[2], goal_circuit_pairs ))
+      goal_set = Set( map( x->x[1], goal_circuit_pairs ) )
+    else
+      circuit_set_previous = deepcopy( circuit_set )
+      circuit_set = Set{Chromosome}[]
+      goal_set = Set{Goal}[]
+      j = 0
+      for c in circuit_set_previous
+        j += 1
+        goal_circuit_pairs = mutate_all( c, funcs, output_chromosomes=true ) 
+        circuit_set = union( Set(map( x->x[2], goal_circuit_pairs )), circuit_set )
+        goal_set = union( Set( map( x->x[1], goal_circuit_pairs ) ), goal_set )
+        #println("j: ",j,"  lengths: cl: ",length(circuit_list),"  gs: ",length(goal_set))
+      end
+    end    
+    println("length(circuit_set): ",length(circuit_set))
+    println("length(goal_set): ",length(goal_set))
+    if i == 1
+      push!(goal_sets,goal_set)
+    else
+      push!(goal_sets,union(goal_set,goal_sets[i-1]))
+      println("i: ",i,"  length(goal_sets[i]): ",length(goal_sets[i]))
+    end
+  end
+  goal_sets 
+end
+    
+function run_mutational_evolvability( g::Goal, p::Parameters, num_mutations::Integer, max_steps::Integer,  max_attempts::Integer)
+  nc = nothing
+  attempt = 0
+  steps = 0
+  c = random_chromosome(p,default_funcs(p.numinputs))
+  while attempt < max_attempts && steps == max_steps
+    attempt += 1
+    (nc,steps) = neutral_evolution( c, g, max_steps, max_attempts )
+  end
+  gs=mutational_evolvability(c, num_mutations )
+  length(gs[end])
+end
+    
+function run_mutational_evolvability( gl::GoalList, p::Parameters, num_mutations::Integer, max_steps::Integer,  max_attempts::Integer;
+    csvfile::String="" )
+  geno_counts = pmap( g->run_mutational_evolvability( g, p, num_mutations, max_steps, max_attempts ), gl )
+  df = DataFrame()
+  df.goal = gl
+  df.geno_counts = geno_counts
+  if length(csvfile) > 0
+    open( csvfile, "w" ) do f
+      hostname = chomp(open("/etc/hostname") do f read(f,String) end)
+      println(f,"# date and time: ",Dates.now())
+      println(f,"# host: ",hostname," with ",nprocs()-1,"  processes: " )
+      println(f,"# funcs: ", Main.CGP.default_funcs(p.numinputs))
+      print_parameters(f,p,comment=true)
+      println(f,"# num_mutations: ",num_mutations)
+      println(f,"# max_steps: ",max_steps)
+      println(f,"# max_attempts: ",max_attempts)
+      CSV.write( f, df, append=true, writeheader=true )
+    end
+  end
+  df
+  df
+end
