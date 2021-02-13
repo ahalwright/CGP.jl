2bf8a84150e97f83e4c5ad99cc5db22c8246e6a0
Alden Wright <github@fastmail.us>
2021-02-13 10:41:52 -0700

    Revisions to mutational_evolvability.jl.

--- src/mutational_evolvability.jl
+++ src/mutational_evolvability.jl
@@ -3,8 +3,73 @@ using CSV
 using Dates
 
 # Compute the number of unique phenotypes in neighborhoods of increasing mutational size of a given genotype.
-# Hypothesis:  This increases more rapidly for complex genotypes than simple genotypes.
+# Hypothesis:  Number unique phenotypes is postively related to complexity and 
+#   negatively related to robustness.
 
+# Parallel map run_mutational_evolvability() over the goals of gl.  Output a dataframe csv file.
+# Note that goals in gl may be replaced by random goals if no circuit is evolved to map to a goal.
+function run_mutational_evolvability( gl::GoalList, p::Parameters, num_mutations::Integer, max_steps::Integer,  max_attempts::Integer;
+    csvfile::String="" )
+  gl_sav = deepcopy(gl)
+  println("gl: ",gl)
+  #complexity_robustness_geno_counts = map( g->mutational_evolvability( g, p, num_mutations, max_steps, max_attempts ), gl )
+  complexity_robustness_geno_counts = pmap( g->mutational_evolvability( g, p, num_mutations, max_steps, max_attempts ), gl )
+  println("length pairs: ",length(complexity_robustness_geno_counts))
+  df = DataFrame()
+  df.goal = map(x->x[1],complexity_robustness_geno_counts)
+  df.complexity = map(x->x[2],complexity_robustness_geno_counts)
+  df.robusness = map(x->x[3],complexity_robustness_geno_counts)
+  for i = 1:num_mutations
+    insertcols!(df,size(df)[2]+1,Symbol("geno_count$i")=>map(x->x[i+3],complexity_robustness_geno_counts))
+  end
+  if length(csvfile) > 0
+    open( csvfile, "w" ) do f
+      hostname = chomp(open("/etc/hostname") do f read(f,String) end)
+      println(f,"# date and time: ",Dates.now())
+      println(f,"# host: ",hostname," with ",nprocs()-1,"  processes: " )
+      println(f,"# funcs: ", Main.CGP.default_funcs(p.numinputs))
+      print_parameters(f,p,comment=true)
+      println(f,"# num_mutations: ",num_mutations)
+      println(f,"# max_steps: ",max_steps)
+      println(f,"# max_attempts: ",max_attempts)
+      CSV.write( f, df, append=true, writeheader=true )
+    end
+  end
+  df
+end
+    
+# Run the below defined mutational_evolvability() for a single goal.  
+# Use neutral_evolution() to find the genotype that maps to the given goal (phenotype).
+# If no circuit can be evolved that maps to g, then g is replace be a random goal (phenotype).
+function mutational_evolvability( g::Goal, p::Parameters, num_mutations::Integer, max_steps::Integer,  max_attempts::Integer;
+      max_goal_attempts::Int64=10)
+  println("mut evolve g: ",g)
+  nc = nothing
+  goal_attempt = 0
+  while goal_attempt < max_goal_attempts
+    attempt = 0
+    steps = max_steps
+    while attempt < max_attempts && steps == max_steps
+      c = random_chromosome(p,default_funcs(p.numinputs))
+      attempt += 1
+      (nc,steps) = neutral_evolution( c, g, max_steps )
+      println("after neutral evolution: steps: ",steps)
+    end
+    if steps == max_steps
+      g = randgoal(p.numinputs,p.numoutputs)
+      println("new goal generated: ",g)
+      goal_attempt += 1
+    else 
+      break
+    end
+  end
+  complexity = complexity5(nc)
+  #println("output values(nc): ",output_values(nc),"  complexity: ",complexity)
+  print_circuit(nc)
+  (robustness,goal_sets) = mutational_evolvability(nc, num_mutations )
+  Tuple(vcat([g,complexity,robustness],[length(goal_sets[end-j]) for j = (num_mutations-1):-1:0]))
+end
+    
 function mutational_evolvability( c::Chromosome, num_mutations::Int64 )
   funcs = default_funcs( c.params.numinputs )
   robustness = -1.0   # establish scope
@@ -42,57 +107,4 @@ function mutational_evolvability( c::Chromosome, num_mutations::Int64 )
     end
   end
   (robustness,goal_sets)
-end
-    
-# Run the above defined mutational_evolvability() for a single goal.  
-# Use neutral_evolution() to find the genotype that maps to the given goal (phenotype).
-function mutational_evolvability( g::Goal, p::Parameters, num_mutations::Integer, max_steps::Integer,  max_attempts::Integer)
-  println("mut evolve g: ",g)
-  nc = nothing
-  attempt = 0
-  steps = max_steps
-  c = random_chromosome(p,default_funcs(p.numinputs))
-  while attempt < max_attempts && steps == max_steps
-    attempt += 1
-    (nc,steps) = neutral_evolution( c, g, max_steps )
-    println("after neutral evolution: steps: ",steps)
-  end
-  complexity = complexity5(nc)
-  #println("output values(nc): ",output_values(nc),"  complexity: ",complexity)
-  print_circuit(nc)
-  (robustness,goal_sets) = mutational_evolvability(nc, num_mutations )
-  (complexity,robustness,length(goal_sets[end-2]),length(goal_sets[end-1]),length(goal_sets[end]))
-end
-    
-# Parallel map run_mutational_evolvability() over the goals of gl.  Output a dataframe csv file.
-function run_mutational_evolvability( gl::GoalList, p::Parameters, num_mutations::Integer, max_steps::Integer,  max_attempts::Integer;
-    csvfile::String="" )
-  gl_sav = deepcopy(gl)
-  println("gl: ",gl)
-  complexity_robustness_geno_counts = map( g->mutational_evolvability( g, p, num_mutations, max_steps, max_attempts ), gl )
-  #complexity_robustness_geno_counts = pmap( g->mutational_evolvability( g, p, num_mutations, max_steps, max_attempts ), gl )
-  println("length pairs: ",length(complexity_robustness_geno_counts))
-  df = DataFrame()
-  df.goal = gl_sav
-  df.complexity = map(x->x[1],complexity_robustness_geno_counts)
-  df.robusness = map(x->x[2],complexity_robustness_geno_counts)
-  for i = 1:num_mutations
-    insert!(df,size(df)[2]+1,map(x->x[i+2],complexity_robustness_geno_counts),Symbol("geno_count$i"))
-  end
-  #df.geno_count_endminus1 = map(x->x[3],complexity_robustness_geno_counts)
-  #df.geno_count_end = map(x->x[4],complexity_robustness_geno_counts)
-  if length(csvfile) > 0
-    open( csvfile, "w" ) do f
-      hostname = chomp(open("/etc/hostname") do f read(f,String) end)
-      println(f,"# date and time: ",Dates.now())
-      println(f,"# host: ",hostname," with ",nprocs()-1,"  processes: " )
-      println(f,"# funcs: ", Main.CGP.default_funcs(p.numinputs))
-      print_parameters(f,p,comment=true)
-      println(f,"# num_mutations: ",num_mutations)
-      println(f,"# max_steps: ",max_steps)
-      println(f,"# max_attempts: ",max_attempts)
-      CSV.write( f, df, append=true, writeheader=true )
-    end
-  end
-  df
 end
