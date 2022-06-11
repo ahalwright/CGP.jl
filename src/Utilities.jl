664aef5accd7849df8274ca0a89bc148954cf1d0
Alden Wright <github@fastmail.us>
2022-06-11 10:29:16 -0600

    Updated notes files and minor change to Utilities.jl

--- src/Utilities.jl
+++ src/Utilities.jl
@@ -2,7 +2,7 @@ export write_df_to_csv
 # Writes a dataframe to a CSV file
 function write_df_to_csv( df::DataFrame, p::Parameters, funcs::Vector{Func}, csvfile::String;
     mutrate::Float64=-1.0, ngens::Int64=-1, popsize::Int64=-1, nreps::Int64=-1, max_steps::Int64=-1,
-    numcircuits::Int64=-1, max_mutates::Int64=-1, goal_list::Vector{Vector{MyInt}}=Vector{MyInt}[] )
+    numcircuits::Int64=-1, max_mutates::Int64=-1, nsamples::Int64, goal_list::Vector{Vector{MyInt}}=Vector{MyInt}[] )
   open( csvfile, "w" ) do f
     hostname = chomp(open("/etc/hostname") do f read(f,String) end) 
     println(f,"# date and time: ",Dates.now())
@@ -26,6 +26,9 @@ function write_df_to_csv( df::DataFrame, p::Parameters, funcs::Vector{Func}, csv
     end 
     if numcircuits >= 0
       println(f,"# numcircuits: ",numcircuits)
+    end 
+    if nsamples >= 0
+      println(f,"# nsamples: ",nsamples)
     end 
     if max_mutates >= 0
       println(f,"# max_mutates: ",max_mutates)
--- src/robust_evolution.jl
+++ src/robust_evolution.jl
@@ -1,4 +1,4 @@
-# First written on 5/11/21.  Finally debugged on 5/13/21.
+# First written on 5/11/21.  
 # Given a target phenotype, employs neutral (phenotype doesn't change) evolution to discover increeasingly robust genotypes that map to the target phenotype.
 #using HypothesisTests
 using Random
@@ -6,14 +6,27 @@ using Random
 
 # Returns result_list which is a list of (circuit,rbst) pairs where rbst is the robustness() of the circuit.
 # The first element of the list has maximum robustness.
+# Example run:
+#=
+ include("robust_evolution.jl")
+ p = Parameters(2,1,5,5)
+ funcs = default_funcs(p)
+ phlist = [[0x0005],[0x0000d]]  # target phenotype
+ nreps = 20           # number of steps where we try to improve robustness
+ max_steps = 2000     # maximum number of steps when running function neutral_evolution()
+ max_mutates = 30     # Not used when keyword use_neutral is false
+ max_tries = 10       # maximum number of tries for running neutral evolution
+ df = robust_evolution_parallel( p, ph, nreps, max_steps, max_mutates, max_tries, funcs )
+=#
 function robust_evolution_parallel( p::Parameters, phlist::GoalList, nreps::Int64, max_steps::Integer, max_mutates::Int64, max_tries::Int64, funcs::Vector{Func};
       use_neutral::Bool=true, use_lincircuit::Bool=false, csvfile::String="" )
+  sort!(phlist,by=x->x[1])
   if use_neutral
-    #result_list = pmap( ph->robust_evolution_neutral( p, ph, nreps, max_steps, max_tries, funcs, use_lincircuit=use_lincircuit ), phlist )
-    result_list = map( ph->robust_evolution_neutral( p, ph, nreps, max_steps, max_tries, funcs, use_lincircuit=use_lincircuit ), phlist )
+    result_list = pmap( ph->robust_evolution_neutral( p, ph, nreps, max_steps, max_tries, funcs, use_lincircuit=use_lincircuit ), phlist )
+    #result_list = map( ph->robust_evolution_neutral( p, ph, nreps, max_steps, max_tries, funcs, use_lincircuit=use_lincircuit ), phlist )
   else
-    #result_list = pmap( ph->robust_evolution( p, ph, nreps, max_steps, max_mutates, max_tries, funcs, use_lincircuit=use_lincircuit ), phlist )
-    result_list = map( ph->robust_evolution( p, ph, nreps, max_steps, max_mutates, max_tries, funcs, use_lincircuit=use_lincircuit ), phlist )
+    result_list = pmap( ph->robust_evolution( p, ph, nreps, max_steps, max_mutates, max_tries, funcs, use_lincircuit=use_lincircuit ), phlist )
+    #result_list = map( ph->robust_evolution( p, ph, nreps, max_steps, max_mutates, max_tries, funcs, use_lincircuit=use_lincircuit ), phlist )
   end
   df = DataFrame()
   df.goal = String[]
@@ -30,6 +43,71 @@ function robust_evolution_parallel( p::Parameters, phlist::GoalList, nreps::Int6
     write_df_to_csv(df,p,funcs,csvfile,nreps=nreps,max_steps=max_steps,max_mutates=max_mutates)
   end
   df
+end
+
+# This version does a random phenotype preseriving walk starting with a single circuit that maps to the given phenotype ph 
+# returns a (circuit,rbst) pair where rbst is the robustness() of the circuit. 
+# Example run:
+#=
+ include("robust_evolution.jl")
+ p = Parameters(2,1,5,5)
+ funcs = default_funcs(p)
+ nreps = 20        # number of steps where we try to improve robustness
+ max_steps = 2000  # maximum number of steps when running function neutral_evolution()
+ max_tries = 10    # maximum number of tries for running neutral evolution
+ ph = [0x0005]     # target phenotype
+ (circ, start_rbst, end_rbst) = robust_evolution_neutral( p, ph, nreps, max_steps, max_tries, funcs );
+ print_circuit(circ)
+ (start_rbst,end_rbst)
+=#
+function robust_evolution_neutral( p::Parameters, ph::Goal, nreps::Int64, max_steps::Integer, max_tries::Int64, funcs::Vector{Func}; use_lincircuit::Bool=false )
+  tries = 0
+  done = false
+  nc = c = use_lincircuit ? rand_lcircuit( p, funcs ) : random_chromosome( p, funcs )
+  while !done && tries < max_tries
+    #println("ph: ",ph,"  c: ",c)
+    (nc,steps) = neutral_evolution( c, ph, max_steps )
+    tries += 1
+    if steps < max_steps
+      done = true
+    end
+    c = use_lincircuit ? rand_lcircuit( p, funcs ) : random_chromosome( p, funcs )
+  end
+  if !done 
+    error("neutral evolution failed in function robust_evolution() ")
+  end
+  @assert ph == output_values(nc)
+  rbst = robustness(c,funcs)
+  starting_robustness = rbst
+  print("step: ",0,"  ph: ",ph,"  rbst: ",rbst,"   ")
+  #print_circuit(c,funcs)
+  for rep = 1:nreps
+    new_c = deepcopy(c)
+    if typeof(c)==LinCircuit
+      mutate_circuit!( new_c, funcs )
+    else
+      mutate_chromosome!(new_c,funcs)
+    end
+    new_rbst = robustness( new_c, funcs )
+    new_ov = output_values( new_c )
+    #=
+    if new_ov == ph
+      #println("B rep: ",rep,"  ph: ",ph,"  new ov: ",output_values(new_c),"  new_rbst: ",new_rbst)
+      #println("start robust: ",starting_robustness)
+    end
+    =#
+    if new_ov != ph || new_rbst < rbst
+      continue
+    end
+    #println("A rep: ",rep,"  ph: ",ph,"  new ov: ",output_values(new_c),"  new_rbst: ",new_rbst)
+    c = new_c
+    @assert output_values(c) == ph
+    rbst = new_rbst
+    #print("step: ",step,"  ov: ",output_values(c),"  rbst: ",rbst,"   ")
+    #println("start robust: ",starting_robustness)
+    #print_circuit(c,funcs)
+  end
+  (c,starting_robustness,rbst)
 end
 
 # This version evolves many genotypes that map to the given phenotype ph and thus is not an example of doing a random phenotype-preserving walk starting at a given genotype
@@ -84,51 +162,6 @@ function robust_evolution( p::Parameters, ph::Goal, nreps::Int64, max_steps::Int
   sort!(circ_rbst_list,by=x->x[2])  # sort on robustness
   (circ,best_rbst) =  circ_rbst_list[end]        # return the pair with the largest robustness
   (circ,average_starting_robustness,best_rbst)
-end
-
-# This version does a random phenotype preseriving walk starting with a single circuit that maps to the given phenotype ph 
-# returns a (circuit,rbst) pair where rbst is the robustness() of the circuit. 
-function robust_evolution_neutral( p::Parameters, ph::Goal, nreps::Int64, max_steps::Integer, max_tries::Int64, funcs::Vector{Func}; use_lincircuit::Bool=false )
-  tries = 0
-  done = false
-  nc = c = use_lincircuit ? rand_lcircuit( p, funcs ) : random_chromosome( p, funcs )
-  while !done && tries < max_tries
-    #println("ph: ",ph,"  c: ",c)
-    (nc,steps) = neutral_evolution( c, ph, max_steps )
-    tries += 1
-    if steps < max_steps
-      done = true
-    end
-    c = use_lincircuit ? rand_lcircuit( p, funcs ) : random_chromosome( p, funcs )
-  end
-  if !done 
-    error("neutral evolution failed in function robust_evolution() ")
-  end
-  @assert ph == output_values(nc)
-  rbst = starting_robustness = robustness(c,funcs)
-  #print("step: ",0,"  ph: ",ph,"  rbst: ",rbst,"   ")
-  print_circuit(c,funcs)
-  for rep = 1:nreps
-    #(new_c,active) = mutate_chromosome!( deepcopy(c), funcs )
-    if typeof(c)==LinCircuit
-      new_c = mutate_circuit!(deepcopy(c),funcs)
-    else
-      (new_c,active) = mutate_chromosome!(deepcopy(c),funcs)
-    end
-    new_rbst = robustness( new_c, funcs )
-    new_ov = output_values( new_c )
-    #println("rep: ",rep,"  new ov: ",output_values(new_c),"  new_rbst: ",new_rbst)
-    if new_ov != ph || new_rbst < rbst
-      continue
-    end
-    #println("rep: ",rep,"  new ov: ",output_values(new_c),"  new_rbst: ",new_rbst)
-    c = new_c
-    @assert output_values(c) == ph
-    rbst = new_rbst
-    print("step: ",step,"  ov: ",output_values(c),"  rbst: ",rbst,"   ")
-    #print_circuit(c,funcs)
-  end
-  (c,starting_robustness,rbst)
 end
 
 # Not used
